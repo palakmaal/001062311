6666f3bcb74906877ec21a497ad58da22b5e2d4d
Palak Maal <maal.p@husky.neu.edu>
2020-01-19 21:55:51 -0500

    I have created Assignment1 in the name of Java Application3

---
+++ JavaApplication3/src/Business/Person.java
@@ -0,0 +1,268 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author maalp
+ */
+public class Person {
+    private String FirstName;
+    private String LastName;
+    private String PhoneNumber;
+    private String DateofBirth;
+    private String Age;
+    private String Height;
+    private String Weight;
+    private String SocialSecurityNumber;
+    private String StreetAddress;
+    private String City;
+    private String State;
+    private String ZipCode;
+    private String BankName;
+    private String BankRoutingNumber;
+    private String BankAccountNumber;
+    private String AccountBalance;
+    private String BankNamechecking;
+    private String Bankroutingnumberchecking;
+    private String Bankaccountnumberchecking;
+    private String AccountBalancechecking;
+    private String LicenseNumber;
+    private String IssueDate;
+    private String ExpiryDate;
+    private String BloodType;
+    private String MedicalRecordNumber;
+    private String Alergy1;
+    private String Alergy2;
+    private String Alergy3;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public String getDateofBirth() {
+        return DateofBirth;
+    }
+
+    public void setDateofBirth(String DateofBirth) {
+        this.DateofBirth = DateofBirth;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getHeight() {
+        return Height;
+    }
+
+    public void setHeight(String Height) {
+        this.Height = Height;
+    }
+
+    public String getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(String Weight) {
+        this.Weight = Weight;
+    }
+
+    public String getSocialSecurityNumber() {
+        return SocialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String SocialSecurityNumber) {
+        this.SocialSecurityNumber = SocialSecurityNumber;
+    }
+
+    public String getStreetAddress() {
+        return StreetAddress;
+    }
+
+    public void setStreetAddress(String StreetAddress) {
+        this.StreetAddress = StreetAddress;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZipCode() {
+        return ZipCode;
+    }
+
+    public void setZipCode(String ZipCode) {
+        this.ZipCode = ZipCode;
+    }
+
+    public String getBankName() {
+        return BankName;
+    }
+
+    public void setBankName(String BankName) {
+        this.BankName = BankName;
+    }
+
+    public String getBankRoutingNumber() {
+        return BankRoutingNumber;
+    }
+
+    public void setBankRoutingNumber(String BankRoutingNumber) {
+        this.BankRoutingNumber = BankRoutingNumber;
+    }
+
+    public String getBankAccountNumber() {
+        return BankAccountNumber;
+    }
+
+    public void setBankAccountNumber(String BankAccountNumber) {
+        this.BankAccountNumber = BankAccountNumber;
+    }
+
+    public String getAccountBalance() {
+        return AccountBalance;
+    }
+
+    public void setAccountBalance(String AccountBalance) {
+        this.AccountBalance = AccountBalance;
+    }
+
+    public String getBankNamechecking() {
+        return BankNamechecking;
+    }
+
+    public void setBankNamechecking(String BankNamechecking) {
+        this.BankNamechecking = BankNamechecking;
+    }
+
+    public String getBankroutingnumberchecking() {
+        return Bankroutingnumberchecking;
+    }
+
+    public void setBankroutingnumberchecking(String Bankroutingnumberchecking) {
+        this.Bankroutingnumberchecking = Bankroutingnumberchecking;
+    }
+
+    public String getBankaccountnumberchecking() {
+        return Bankaccountnumberchecking;
+    }
+
+    public void setBankaccountnumberchecking(String Bankaccountnumberchecking) {
+        this.Bankaccountnumberchecking = Bankaccountnumberchecking;
+    }
+
+    public String getAccountBalancechecking() {
+        return AccountBalancechecking;
+    }
+
+    public void setAccountBalancechecking(String AccountBalancechecking) {
+        this.AccountBalancechecking = AccountBalancechecking;
+    }
+
+    public String getLicenseNumber() {
+        return LicenseNumber;
+    }
+
+    public void setLicenseNumber(String LicenseNumber) {
+        this.LicenseNumber = LicenseNumber;
+    }
+
+    public String getIssueDate() {
+        return IssueDate;
+    }
+
+    public void setIssueDate(String IssueDate) {
+        this.IssueDate = IssueDate;
+    }
+
+    public String getExpiryDate() {
+        return ExpiryDate;
+    }
+
+    public void setExpiryDate(String ExpiryDate) {
+        this.ExpiryDate = ExpiryDate;
+    }
+
+    public String getBloodType() {
+        return BloodType;
+    }
+
+    public void setBloodType(String BloodType) {
+        this.BloodType = BloodType;
+    }
+
+    public String getMedicalRecordNumber() {
+        return MedicalRecordNumber;
+    }
+
+    public void setMedicalRecordNumber(String MedicalRecordNumber) {
+        this.MedicalRecordNumber = MedicalRecordNumber;
+    }
+
+    public String getAlergy1() {
+        return Alergy1;
+    }
+
+    public void setAlergy1(String Alergy1) {
+        this.Alergy1 = Alergy1;
+    }
+
+    public String getAlergy2() {
+        return Alergy2;
+    }
+
+    public void setAlergy2(String Alergy2) {
+        this.Alergy2 = Alergy2;
+    }
+
+    public String getAlergy3() {
+        return Alergy3;
+    }
+
+    public void setAlergy3(String Alergy3) {
+        this.Alergy3 = Alergy3;
+    }
+
+   
+    
+}
---
+++ Lab1/src/Business/Product.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+        
+
+/**
+ *
+ * @author maalp
+ */
+public class Product {
+    private String name;
+    private String price;
+    private String Aavailnum;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAavailnum() {
+        return Aavailnum;
+    }
+
+    public void setAavailnum(String Aavailnum) {
+        this.Aavailnum = Aavailnum;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+  
+    
+}
